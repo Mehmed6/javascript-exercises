be8013983d3e7b8faacf46ff222c608ce9aad3e5
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-08-28 00:52:17 +0200

    Create RandomPrimeGenerator.js

---
+++ src/utils/random/RandomPrimeGenerator.js
@@ -0,0 +1,31 @@
+import {randomInt} from "./random.js";
+import {isPrime} from "../numeric/numeric.js";
+
+
+export class RandomPrimeGenerator {
+    constructor(count, min, max, period) {
+        this._count = count
+        this._min = min
+        this._max = max
+        this._period = period
+        this._n = 0
+    }
+
+    _randomGeneratorCallback(action) {
+        const val = randomInt(this._min, this._max + 1)
+
+        if (isPrime(val)) {
+            action(val, true)
+            ++this._n
+        }
+        else
+            action(val, false)
+
+        if (this._count === this._n)
+            clearInterval(this._interval)
+    }
+
+    run(action) {
+        this._interval = setInterval(() => this._randomGeneratorCallback(action), this._period)
+    }
+}
